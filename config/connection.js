2ca3bfa96b2a47a0d62de06dbab116d9984b41e5
cubanerick <37166192+cubanerick@users.noreply.github.com>
2018-06-27 01:19:36 -0700

    final jaws_db connect

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
     host: "127.0.0.1",
     port: 3305,
     user: "root",
     password: "root",
     database: "burgers_db"
-});
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
